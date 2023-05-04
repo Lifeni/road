488e3d04afe0dc8349a4443a83cb9447e18c9b62
Lifeni <liangfengning@foxmail.com>
2023-05-05 00:18:00 +0800

    调整文档

--- src/components/Docs.tsx
+++ src/components/Docs.tsx
@@ -2,7 +2,22 @@ import { Code } from './base/Icons'
 
 const docs = [
   {
+    name: '快捷重定向',
+    hide: false,
+    apis: [
+      {
+        method: 'GET',
+        path: '/+/<URL>?<TYPE>',
+        descriptions: [
+          '快捷创建指定地址的重定向链接。',
+          '使用 ?json 获取 JSON 格式的链接地址文本。',
+        ],
+      },
+    ],
+  },
+  {
     name: '重定向链接',
+    hide: false,
     apis: [
       {
         method: 'GET',
@@ -33,20 +48,8 @@ const docs = [
     ],
   },
   {
-    name: '快捷重定向',
-    apis: [
-      {
-        method: 'GET',
-        path: '/+/<URL>?<TYPE>',
-        descriptions: [
-          '快捷创建指定地址的重定向链接。',
-          '使用 ?json 获取 JSON 格式的链接地址文本。',
-        ],
-      },
-    ],
-  },
-  {
     name: '反向代理',
+    hide: true,
     apis: [
       {
         method: 'GET',
@@ -66,30 +69,35 @@ const maps = {
 
 export const Docs = () => (
   <article>
-    {docs.map(({ name, apis }) => (
-      <section class="Box-row">
-        <h2 class="h5 pb-1 d-flex flex-items-center">
-          {Code}
-          <span class="px-2 flex-1">{name}</span>
-        </h2>
-        <ul class="list-style-none">
-          {apis.map(({ method, path, descriptions }) => (
-            <li class="mt-2">
-              <h3 class="h6 mb-1 pb-1">
-                <code
-                  class={`IssueLabel color-bg-${maps[method]}-emphasis color-fg-on-emphasis mr-1`}
-                >
-                  {method}
-                </code>
-                <code class={`Label mr-1 Label--${maps[method]}`}>{path}</code>
-              </h3>
-              {descriptions.map(description => (
-                <p class="mb-0 f6">{description}</p>
+    {docs.map(
+      ({ name, apis, hide }) =>
+        !hide && (
+          <section class="Box-row">
+            <h2 class="h5 pb-1 d-flex flex-items-center">
+              {Code}
+              <span class="px-2 flex-1">{name}</span>
+            </h2>
+            <ul class="list-style-none">
+              {apis.map(({ method, path, descriptions }) => (
+                <li class="mt-2">
+                  <h3 class="h6 mb-1 pb-1">
+                    <code
+                      class={`IssueLabel color-bg-${maps[method]}-emphasis color-fg-on-emphasis mr-1`}
+                    >
+                      {method}
+                    </code>
+                    <code class={`Label mr-1 Label--${maps[method]}`}>
+                      {path}
+                    </code>
+                  </h3>
+                  {descriptions.map(description => (
+                    <p class="mb-0 f6">{description}</p>
+                  ))}
+                </li>
               ))}
-            </li>
-          ))}
-        </ul>
-      </section>
-    ))}
+            </ul>
+          </section>
+        )
+    )}
   </article>
 )
