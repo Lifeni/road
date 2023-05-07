be1405230dd023d9612dd24aa53baeb111a0313a
Lifeni <liangfengning@foxmail.com>
2023-05-07 22:44:28 +0800

    更换 CSS 文件源

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -19,11 +19,8 @@ const Head = () => (
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="robots" content="noindex" />
-    <link rel="icon" href="/favicon.svg" type="image/x-icon" />
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/@primer/css/dist/primer.css"
-    />
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+    <link rel="stylesheet" href="https://file.lifeni.life/assets/primer.css" />
     <title>随意链接</title>
     {Style}
   </head>
