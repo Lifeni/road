c8aa2ba49ce643d7d7140972e32bc4de6ada8f3d
Lifeni <liangfengning@foxmail.com>
2023-07-01 21:09:44 +0800

    添加域名重定向

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,31 @@
 import { Hono } from 'hono'
-import { serveStatic } from 'hono/cloudflare-workers'
 import { logger } from 'hono/logger'
 import { prettyJSON } from 'hono/pretty-json'
 import { proxy } from './libs/proxy'
 import { redirect } from './libs/redirect'
-import { IndexPage } from './routers'
 
-const app = new Hono()
-export const reserved = [
-  '/',
-  '/ids',
-  '/+',
-  '/-',
-  '/robots.txt',
-  '/favicon.svg',
-  '/favicon.ico',
-]
+const app = new Hono({
+  getPath: req => {
+    const host = req.headers.get('host')
+    if (host?.endsWith('lifeni.life')) return 'lifeni.life/'
+    return req.headers.get('host') + req.url.replace(/^https?:\/\/[^\/]+/, '')
+  },
+})
+
+const domains = ['dev', 'lab', 'server']
+const base = 'https://lifeni.life'
+
+app.all('lifeni.life/', async c => {
+  const host = c.req.headers.get('host')
+  const name = host?.replace('.lifeni.life', '') || ''
+  if (!host || !domains.includes(name)) return c.redirect(`${base}/404`)
+  return c.redirect(`${base}/${name}`)
+})
 
 app.use('*', prettyJSON())
 app.use('*', logger())
 
-app.use('/robots.txt', serveStatic({ path: './robots.txt' }))
-app.use('/favicon.svg', serveStatic({ path: './favicon.svg' }))
-app.use('/favicon.ico', serveStatic({ path: './favicon.ico' }))
-
-app.get('/', c => {
-  const host = new URL(c.req.url).host
-  return c.html(IndexPage({ host }))
-})
-
-app.get('/ids', async c => {
-  const routes = c.env?.routes as KVNamespace
-  const ids = Number(await routes.get('ids')) || 1
-  const json =
-    c.req.query('json') !== undefined || c.req.query('j') !== undefined
-
-  if (json) return c.json({ ids })
-  else return c.text(`${ids}`)
-})
-
-app.post('/', async c => {
-  const form = await c.req.formData()
-  const routes = c.env?.routes as KVNamespace
-  const ids = (Number(await routes.get('ids')) || 1) + 1
-
-  const slug = form.get('slug') || `${ids}`
-  const url = form.get('url')
-  const host = new URL(c.req.url).host
-  const protocol = new URL(c.req.url).protocol
-
-  if (reserved.includes(`/${slug}`) || !url)
-    return c.html(IndexPage({ type: 'error', host }))
-  try {
-    await routes.put(slug, url)
-    if (slug === `${ids}`) await routes.put('ids', slug)
-    return c.html(
-      IndexPage({ type: 'ok', host, url: `${host}/${slug}`, protocol })
-    )
-  } catch (error) {
-    console.error(error)
-    return c.html(IndexPage({ type: 'error', host }))
-  }
-})
-
-app.route('/', redirect)
-app.route('/-', proxy)
+app.route('iokl.link/', redirect)
+app.route('iokl.link/-', proxy)
 
 export default app
--- src/libs/redirect.ts
+++ src/libs/redirect.ts
@@ -1,9 +1,62 @@
 import { Hono } from 'hono'
-import { reserved } from '../app'
-import { ErrorPage } from '../routers/404'
+import { serveStatic } from 'hono/cloudflare-workers'
 import { IndexPage } from '../routers'
+import { ErrorPage } from '../routers/404'
 
 export const redirect = new Hono()
+
+const reserved = [
+  '/',
+  '/ids',
+  '/+',
+  '/-',
+  '/robots.txt',
+  '/favicon.svg',
+  '/favicon.ico',
+]
+
+redirect.use('/robots.txt', serveStatic({ path: './robots.txt' }))
+redirect.use('/favicon.svg', serveStatic({ path: './favicon.svg' }))
+redirect.use('/favicon.ico', serveStatic({ path: './favicon.ico' }))
+
+redirect.get('/', c => {
+  const host = new URL(c.req.url).host
+  return c.html(IndexPage({ host }))
+})
+
+redirect.get('/ids', async c => {
+  const routes = c.env?.routes as KVNamespace
+  const ids = Number(await routes.get('ids')) || 1
+  const json =
+    c.req.query('json') !== undefined || c.req.query('j') !== undefined
+
+  if (json) return c.json({ ids })
+  else return c.text(`${ids}`)
+})
+
+redirect.post('/', async c => {
+  const form = await c.req.formData()
+  const routes = c.env?.routes as KVNamespace
+  const ids = (Number(await routes.get('ids')) || 1) + 1
+
+  const slug = form.get('slug') || `${ids}`
+  const url = form.get('url')
+  const host = new URL(c.req.url).host
+  const protocol = new URL(c.req.url).protocol
+
+  if (reserved.includes(`/${slug}`) || !url)
+    return c.html(IndexPage({ type: 'error', host }))
+  try {
+    await routes.put(slug, url)
+    if (slug === `${ids}`) await routes.put('ids', slug)
+    return c.html(
+      IndexPage({ type: 'ok', host, url: `${host}/${slug}`, protocol })
+    )
+  } catch (error) {
+    console.error(error)
+    return c.html(IndexPage({ type: 'error', host }))
+  }
+})
 
 redirect.get('/:slug', async c => {
   const slug = c.req.param('slug')
